801c527c60a4aa63404abfb740b25058617bea3b
GCRY <2381165671@qq.com>
2025-03-14 22:46:56 +0800

    boom函数

--- BOOM.py
+++ BOOM.py
@@ -1,3 +1,7 @@
-BOOM=["BOOM","BANG","BAN","POW","POOF","PAH"]
-while True:
-    print(*BOOM)
+def BOOM():
+    BOOM=["BOOM","BANG","BAN","POW","POOF","PAH"]
+    while True:
+        print(*BOOM)
+
+if __name__ == '__main__':
+    BOOM()
