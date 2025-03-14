b595e88e9e7233f43b6e1be9b80db50fb16b8b5b
GCRY6 <2381165671@qq.com>
2025-03-14 22:59:04 +0800

    更加严谨

--- yuan.py
+++ yuan.py
@@ -1,5 +1,8 @@
-import turtle as t 
-for i in range(72):
-    t.forward(10)
-    t.left(5)
-t.done()
+def yuan():
+    import turtle as t 
+    for i in range(72):
+        t.forward(10)
+        t.left(5)
+    t.done()
+if __name__ == '__main__':
+    yuan()
